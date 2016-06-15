e7a2c3c2fe1ba8686f8d5c4d5c4a472058e7bd7a
Toni Saarela <tn.saarela.dev@gmail.com>
2016-06-15 13:03:19 +0200

    add webview to main activity

--- app/src/main/java/com/worldstar/webviewlocker/MainActivity.java
+++ app/src/main/java/com/worldstar/webviewlocker/MainActivity.java
@@ -2,12 +2,34 @@ package com.worldstar.webviewlocker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // WebView url constant.
+    final String WEB_URL = "http://www.msn.com";
+
+    WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.webView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        loadWebViewContent();
+    }
+
+    private void loadWebViewContent() {
+        if (mWebView != null) {
+            mWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+            mWebView.loadUrl(WEB_URL);
+        }
     }
 }
