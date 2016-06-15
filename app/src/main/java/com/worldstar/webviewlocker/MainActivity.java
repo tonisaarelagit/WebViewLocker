e91a49a7b3fa17ac6466a338047559e608ac5718
Toni Saarela <tn.saarela.dev@gmail.com>
2016-06-15 14:10:16 +0200

    update action bar items

--- app/src/main/java/com/worldstar/webviewlocker/MainActivity.java
+++ app/src/main/java/com/worldstar/webviewlocker/MainActivity.java
@@ -2,6 +2,8 @@ package com.worldstar.webviewlocker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +18,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Setup Action Bar.
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+        }
+
         mWebView = (WebView) findViewById(R.id.webView);
     }
 
@@ -24,6 +32,24 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
 
         loadWebViewContent();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu_main; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_close:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void loadWebViewContent() {
